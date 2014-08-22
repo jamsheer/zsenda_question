01f9b8a76bc7e0f3ee5ebe1932636971798b6e4d
syedsuhail <ssuhail.ahmed93@gmail.com>
2014-08-22 12:15:22 +0530

    Add test file

---
+++ test_zsenda.py
@@ -0,0 +1,27 @@
+from zsenda import Mars
+import pytest
+
+
+def test_init():
+    mars = Mars('jsonfile.json', 'jsonfile2.json')
+    assert mars
+
+def test_send():
+    mars = Mars('jsonfile.json', 'jsonfile2.json')
+    jdict = {'name':'Suhail', 'age':'21', 'place':'Palakkad'}
+    assert mars.send(jdict) == 1
+    assert mars.send('sd') ==None
+    
+    
+
+def test_receive():
+    mars = Mars('jsonfile.json', 'jsonfile2.json')
+    data = mars.receive()
+    assert data['name'] == 'Suhail' 
+    cars = Mars('sa','sda')
+    assert cars.receive() == None
+    
+    
+    
+
+
